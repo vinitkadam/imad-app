a9035eafe6cd1d7a2b594d70e7310fad0deae8eb
vinitkadam <vinitkadam1997@gmail.com>
2017-08-11 20:52:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.send('article one requested and will be sent');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req,res){
