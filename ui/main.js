54a98f25ad9c71da3910bff57a7d01b3a2140c99
vinitkadam <vinitkadam1997@gmail.com>
2017-08-12 23:50:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+    counter = counter+1;
+    document.getElementById('count').innerHtml = counter;
+}
