48f09ffa778d04bc490318b2ef2626b734bec725
vinitkadam <vinitkadam1997@gmail.com>
2017-08-12 23:51:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
     counter = counter+1;
-    document.getElementById('count').innerHtml = counter;
+    document.getElementById('count').innerHtml = counter.toString;
 }
