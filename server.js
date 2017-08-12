b6032c4976333d44eb833660c78ea09d469024e0
vinitkadam <vinitkadam1997@gmail.com>
2017-08-12 21:07:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/:articleName', function(req,res){
 });
 
 var counter = 0;
-app.get('/counter',function(req,res){
+app.get('/counter', function(req,res){
     counter = counter + 1;
     res.send(counter.toString());
 });
