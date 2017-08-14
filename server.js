5c3b11ee30b48ab67bc8ca5cd6c59beece9da87d
vinitkadam <vinitkadam1997@gmail.com>
2017-08-14 21:26:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 var articles = {
     'article-one': {
         title: 'Article One | Vinit Kadam',
