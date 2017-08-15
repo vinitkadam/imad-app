6b4f1b634f4f197568bce621e43700d8954599e1
vinitkadam <vinitkadam1997@gmail.com>
2017-08-15 12:40:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,22 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    //create a request
+    var request = new XMLHttpRequest();
+    
+    //capture the reponse and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();            
+            }
+        }
+    };
+    
+    //Make a request
+    request.open('GET','http://vinitkadam1997.imad.hasura-app.io/counter',false);
+    request.send(null);
 };
