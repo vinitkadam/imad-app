840b55104b34368faab8bae136498b463ce54fe9
vinitkadam <vinitkadam1997@gmail.com>
2017-08-15 14:46:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ button.onclick = function(){
 
 //Submit name
 var nameInput = document.getElementById('name');
-var name = nameInput.value;
+name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function() {
