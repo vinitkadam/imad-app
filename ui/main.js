2da2c3fd31453559f543c6638750b5f5ddef9667
vinitkadam <vinitkadam1997@gmail.com>
2017-08-15 14:41:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,18 +21,36 @@ button.onclick = function(){
     request.send(null);
 };
 
+//Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
+
 submit.onclick = function() {
-    //make arequest to server
+    
+    //create a request
+    var request = new XMLHttpRequest();
+    
+    //capture the reponse and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var namelist = document.getElementById('namelist');
+                namelist.innerHTML = list;         
+            }
+        }
+    };
+    
+    //Make a request
+    request.open('GET','http://vinitkadam1997.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
     
     //capture a list of names and render it as a list
-    var names = ['name1','name2','name3'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var namelist = document.getElementById('namelist');
-    namelist.innerHTML = list;
+    
 };
