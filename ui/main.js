3e07b4c336f736a76a51f0a41a25c37e1be68ab0
vinitkadam <vinitkadam1997@gmail.com>
2017-08-15 14:11:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,19 @@ button.onclick = function(){
     request.open('GET','http://vinitkadam1997.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick() = function() {
+    //make arequest to server
+    
+    //capture a list of names and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var namelist = document.getElementById('namelist');
+    namelist.innerHTML = list;
+}
