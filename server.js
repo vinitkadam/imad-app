69086e5aa37c310428882548e452ad6c9a3495e1
vinitkadam <vinitkadam1997@gmail.com>
2017-08-23 13:22:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ function template(data){
             <head>
             	<title>${title}</title>
             	<meta name="viewport" content="width=device-width, initial-scale=1">
-                <link href="ui/style.css" rel="stylesheet" />
+                <link href="/ui/style.css" rel="stylesheet" />
             </head>
             
             <body>
