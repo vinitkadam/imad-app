a8d39473b37364f6418c400f6c5e8e4f85bcb4ae
vinitkadam <vinitkadam1997@gmail.com>
2017-08-23 15:27:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/submit-name',function(req,res){
 
 app.get('/articles/:articleName', function(req,res){
     
-    pool.query("SELECT * FROM article WHERE title ='"+req.params.articleName+"'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
