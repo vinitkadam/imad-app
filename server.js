501856703e5afc60051ff04e4351d0dac2731180
vinitkadam <vinitkadam1997@gmail.com>
2017-08-27 23:28:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.get('/articles/:articleName', function(req,res){
 function hash(input,salt){
     
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ["pbkf2","10000",salt,hashed.toString('hex')].join('$');
     
 }
 
