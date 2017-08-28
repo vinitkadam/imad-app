f5c86c45a0091033aaaa9daecbad20cf18c2faf4
vinitkadam <vinitkadam1997@gmail.com>
2017-08-28 14:35:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
-
+var bodyParser = require('body-parser');
 
 var Pool = require('pg').Pool; //include Pool to run queries
 
